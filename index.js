fcce15cb3bd8ed50c87a16c3f5d5ef4612fa94b9
OlhaBnd <bundaola185@gmail.com>
2025-04-16 23:54:25 +0300

    оновила сайт

--- index.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('.card-number-input').oninput = () =>{
-    document.querySelector('.card-number-box').innerText = document.querySelector('.card-number-input').value;
-}
-
-document.querySelector('.card-holder-input').oninput = () =>{
-    document.querySelector('.card-holder-name').innerText = document.querySelector('.card-holder-input').value;
-}
