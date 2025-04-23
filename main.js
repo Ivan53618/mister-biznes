93175e92ac6f8ae76dabe488e45d65c473a364dd
Ivan53618 <vanyapopchuk@gmail.com>
2025-04-23 14:10:32 +0300

    зробив головну сторінку

--- main.js
+++ main.js
@@ -1,10 +1,3 @@
-document.querySelector('.card-number-input').oninput = () =>{
-    document.querySelector('.card-number-box').innerText = document.querySelector('.card-number-input').value;
-}
-
-document.querySelector('.card-holder-input').oninput = () =>{
-    document.querySelector('.card-holder-name').innerText = document.querySelector('.card-holder-input').value;
-}
 // Анімація слотів на головній сторінці
 document.addEventListener('DOMContentLoaded', function() {
     // Анімація головних слотів
