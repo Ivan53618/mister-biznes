47311dce1111b3350e8e8212048a83db31b9c5a5
OlhaBnd <bundaola185@gmail.com>
2025-05-01 19:59:18 +0300

    edx

--- fire.js
+++
@@ -1,7 +0,0 @@
-const coins = localStorage.getItem("coins") || 100;
-const earned = localStorage.getItem("earned") || 0;
-const lost = localStorage.getItem("lost") || 0;
-
-document.getElementById("coins").textContent = coins;
-document.getElementById("earned").textContent = earned;
-document.getElementById("lost").textContent = lost;
--- gift.js
+++
@@ -1,2 +0,0 @@
-const coins = localStorage.getItem("coins") || 100;
-    document.getElementById("balance").textContent = coins;
